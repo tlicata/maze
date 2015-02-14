e711f46955a79f695a5ed286f5c3fc0b82750b9a
Tim Licata <tim@timothylicata.com>
2015-02-13 19:24:37 -0800

    grid function to generate walls

--- src/maze.cljs
+++ src/maze.cljs
@@ -3,3 +3,12 @@
 (enable-console-print!)
 
 (println "Hello, World!")
+
+(defn grid [w h]
+  (set (concat
+        ;; horizontal walls
+        (for [x (range (dec w)) y (range h)]
+          #{[x y] [(inc x) y]})
+        ;; vertical walls
+        (for [x (range w) y (range (dec h))]
+          #{[x y] [x (inc y)]}))))
